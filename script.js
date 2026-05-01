f03909328b39f4afb1904350d7f84e71f1902186
natumichan <natumi@example.com>
2026-05-01 12:51:08 +0900

    アドバイス

--- script.js
+++ script.js
@@ -8,9 +8,33 @@ const fortunes =[
 
 const button =document.getElementById("omikuji-bt");
 const result =document.getElementById("result");
+const advice =document.getElementById("advice");
 
 button.addEventListener("click",function () {
     const randomNumber=Math.floor(Math.random()*fortunes.length);
+    const resultText =fortunes[(randomNumber)];
 
     result.textContent=fortunes[(randomNumber)];
+
+    if (resultText.includes("大吉")) {
+        result.style.color = "blue";
+       advice.textContent= "今すぐに行動したほうがヨイ"
+    }else if (resultText.includes("中吉")) {
+        result.style.color = "green";
+
+    }else if (resultText.includes("小吉")) {
+        result.style.color = "orange";
+         
+    }else if (resultText.includes("凶")) {
+        result.style.color = "yellow";
+
+    }else if (resultText.includes("大凶")) {
+        result.style.color = "rainbow";
+      advice.textContent="おやすみ"
+            
+            
+            
+
+    }
+
 });
